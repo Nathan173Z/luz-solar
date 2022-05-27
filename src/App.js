a061d4531729c49f4aa4b849e5dbb5cf470b5b62
unknown <ns77362@gmail.com>
2022-05-27 17:54:44 -0300

    começando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import {HouseLine, Sun, MoonStars} from "phosphor-react";
+import { useEffect, useState } from "react";
 import './App.css';
 
-function App() {
+
+const KWH = 1.57;
+
+export default function SolarSystem() {
+  const [hour, setHour] = useState(6);
+  const [isActive, setIsActive] = useState(false);
+  const [energy, setEnergy] = useState(0);
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setHour(hour + 1);
+    }, 1000);
+
+    if (hour > 23) {
+      setHour(0);
+    }
+
+    return () => clearInterval(interval);
+  }, [hour]);
+
+  useEffect(() => {
+    if (hour > 5 && hour < 19) {
+      setIsActive(true);
+      setEnergy(energy + KWH);
+    } else {
+      setIsActive(false);
+    }
+
+    if (hour === 24) {
+      setEnergy(0);
+    }
+  }, [hour]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Geração de Energia Solar </h1>
+      <h2>Hora do dia: {hour}h </h2>
+      {isActive ? <Sun size={80} color="#ffdd00" weight="fill" /> :<MoonStars size={80} color="#00065c" weight="fill" />}
+      <div>
+      <HouseLine size={96} color="#545454" weight="duotone" />
+        <p>Quantidade de energia gerada no dia: {energy.toFixed(2)} kw/h</p>
+      </div>
     </div>
   );
 }
-
-export default App;
